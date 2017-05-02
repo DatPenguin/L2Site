8d585206b102483797137c4013bb088991e3196d
kalahel <mathieu95480@gmail.com>
2017-05-02 15:50:55 +0200

    Ajout de bars graphs

---
+++ barGraph.php
@@ -0,0 +1,65 @@
+<?php
+	
+	require_once("includes/parser.inc.php");
+	require_once("includes/util.inc.php");
+	require_once("includes/findmyschool.inc.php");
+//	include_once("includes/header.inc.php");
+	require_once ('jpgraph-4.0.2/src/jpgraph.php');
+	require_once ('jpgraph-4.0.2/src/jpgraph_bar.php');
+
+	
+	@session_start();
+
+	$stringTab = array();
+	$dataTab = array();
+	$stringTab = fillStringTab(17);
+			
+	// On comptera le nombre d'établissement par académie et on en ferra un tableau de données
+	for($index = 0; $index < count($stringTab); $index++) {
+		$dataTab[$index] = countElementsFromData($stringTab[$index], 17);				
+	}
+			
+
+	$datay=$dataTab;
+ 
+	// Size of graph
+	$width=900;
+	$height=500;
+ 
+	// Set the basic parameters of the graph
+	$graph = new Graph($width,$height);
+	$graph->SetScale('textlin');
+	 
+	$top = 60;
+	$bottom = 30;
+	$left = 150;
+	$right = 30;
+	$graph->Set90AndMargin($left,$right,$top,$bottom);
+	 
+	// Nice shadow
+	$graph->SetShadow();
+	 
+	// Setup labels
+	$lbl = $stringTab;
+	$graph->xaxis->SetTickLabels($lbl);
+	 
+	// Label align for X-axis
+	$graph->xaxis->SetLabelAlign('right','center','right');
+	 
+	// Label align for Y-axis
+	$graph->yaxis->SetLabelAlign('center','bottom');
+	 
+	// Titles
+	//$graph->title->Set('Number of incidents');
+	 
+	// Create a bar pot
+	$bplot = new BarPlot($datay);
+	$bplot->SetFillColor('orange');
+	$bplot->SetWidth(0.5);
+	$bplot->SetYMin(0);
+	 
+	$graph->Add($bplot);
+	 
+	$graph->Stroke();
+
+?>
--- graph.php
+++ graph.php
@@ -10,13 +10,16 @@
 	create_header("Graphiques");
 	@session_start();
 ?>
-		<h1>Graphs</h1>
+	<h1>Graphs</h1>
 
-		<article>
-			<h3>Répartitions des établissement en fonction des régions</h3>
-			<p>Les 0% affichés ne veulent pas dire qu'il n'y a pas d'établissement dans cette région, uniquement qu'ils constituent moins de 1% du total des établissements </p>
-			<img src="3dGraph.php" alt="Graph 3d"/>
-						
+	<article>
+		<h3>Répartitions des établissements en fonction des régions</h3>
+		<p>Les 0% affichés ne veulent pas dire qu'il n'y a pas d'établissement dans cette région, uniquement qu'ils constituent moins de 1% du total des établissements </p>
+		<img src="3dGraph.php" alt="Graph 3d"/>						
+	</article>
+	<article>
+		<h3>Nombre d'établissements en fonction des académies</h3>
+		<img src="barGraph.php" alt="bar Graph"/>						
 	</article>
 		
 <?php
