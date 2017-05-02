fd7d91949dc6e8e6ee2b3eeaa8992046ef189a07
kalahel <mathieu95480@gmail.com>
2017-05-02 13:18:48 +0200

    Ajout page graph

---
+++ graph.php
@@ -0,0 +1,15 @@
+<?php
+	require_once("includes/parser.inc.php");
+	require_once("includes/util.inc.php");
+	require_once("includes/findmyschool.inc.php");
+	include_once("includes/header.inc.php");
+	create_header("Graphiques");
+	@session_start();
+?>
+		<h1>Tri par ville</h1>
+		<?php
+
+		?>
+<?php
+	include_once("includes/footer.inc.php");
+?>
